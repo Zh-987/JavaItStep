65db87ed238e19c214a7d1e4b14c9b963c0551f3
77001803540 <zhasulanasainov@gmail.com>
2023-03-30 18:17:31 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+      /*  int age;
+        char gender;
+        String name;
+        float height;
+        double weight;
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter your name:");
+        name = in.nextLine();
+        System.out.print("please, Enter your age: ");
+        age = in.nextInt();
+        System.out.print("please select your gender M or W:");
+        gender = in.next().charAt(0);
+        System.out.print("Enter your height:");
+        height = in.nextFloat();
+        System.out.print("Enter your weight:");
+        weight = in.nextDouble();
+
+        in.close();
+
+        System.out.printf("name = %s  age = %d  gender = %c   height = %f  weight = %f", name, age, gender, height, weight);*/
+
+        int a = 300;
+        byte b = (byte) a;
+        System.out.print(b);
+        // %f
+        // %x
+        // %e
+        // %c
+        // %s
+        // %d
+
+
+        // == !=  < > <= >=  |  & !  ^  && ||    a-d+x     +=  -=  /=  *=  %=
+
+
+        // if else     if()  else if() else if()  else ()    switch(){ case 1: }    x<y? (x+y) : (x-y)
+
+
+        // for(int i=0; i<10; i++)
+        // do{} while()
+        // while(){}
+
+    }
+}
